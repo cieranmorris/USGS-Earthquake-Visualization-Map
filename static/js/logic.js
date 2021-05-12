bea486d0506a284dc7850147856694fa52b684ee
Ciera Morris <cieramorris@Cieras-iMac.lan>
2021-05-12 11:47:14 -0600

    troubleshooting mapbox loading

--- static/js/logic.js
+++ static/js/logic.js
@@ -5,10 +5,12 @@ var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/signif
 
 //Perform a GET request to the query URL
 d3.json(queryUrl).then(function(data) {
+    console.log(data)
     //send the data.features object to the createFeatures function
     createFeatures(data.features);
 });
 
+//DOUBLE CHECK THE LAYOUT OF THIS FUNCTION
 function createFeatures(earthquakeData) {
 
     // Define a function we want to run once for each feature in the features array
